f645bfc23a5fbbcd2ebe653e3b2842b612c81f9e
gemdude46 <gemdude46@gmail.com>
2016-03-20 09:27:27 +0000

    yolo

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, redirect
 import os
 
+f = open('index.htm','r')
+indexpage = f.read()
+f.close()
+
+
 class server(Flask):
     def __init__(self, *args, **kwargs):
         super(server, self).__init__(*args, **kwargs)
 
 app = server(__name__)
+
+@app.route('/')
+def index():
+    return indexpage
 
 @app.route('/reboot')
 def reboot():
